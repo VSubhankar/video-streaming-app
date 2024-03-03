283f6ec676d4e37aef5afd2e7074645c4639ee25
VSubhankar <venkumahantisubhankar@gmail.com>
2024-03-03 21:38:13 +0530

    Mobile View

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const showMenu = (headerToggle, navbarId) => {
-    const toggleBtn = document.getElementById(headerToggle),
-      nav = document.getElementById(navbarId)
- 
-    if (headerToggle && navbarId) {
-      toggleBtn.addEventListener('click', () => {
-        nav.classList.toggle('show-menu')
-        toggleBtn.classList.toggle('fa-times')
-      })
-    }
-  }
-  showMenu('header-toggle', 'navbar')
- 
-  const linkcolor = document.querySelectorAll('.nav_link');
- 
-  function colorLink() {
-    linkcolor.forEach(l => l.classList.remove('active'))
-    this.classList.add('active')
-  }
-  linkcolor.forEach(l => l.addEventListener('click', colorLink))
+// Function to show/hide the menu
+function toggleMenu() {
+  const nav = document.getElementById('navbar');
+  nav.classList.toggle('show-menu');
+}
+
+// Function to set active link and close the menu
+function setActiveLink(element) {
+  const linkColor = document.querySelectorAll('.nav_link');
+  linkColor.forEach((l) => l.classList.remove('active'));
+  element.classList.add('active');
+
+  // Close the menu when a link is clicked
+  const nav = document.getElementById('navbar');
+  nav.classList.remove('show-menu');
+}
